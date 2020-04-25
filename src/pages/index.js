9486ac69431eafbeec2a1600b1e01674bcf43717
datejer <ejer.priv@wp.pl>
2020-04-25 23:11:26 +0200

    Create basic layout, styles

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,49 @@
 import React from "react"
-import { Link } from "gatsby"
+import styled from "styled-components"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
+import logo from "../images/logo.png"
+
+const Container = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  @media (max-width: 600px) {
+    flex-direction: column;
+  }
+`
+
+const Name = styled.h1`
+  font-family: "Lexend Giga", sans-serif;
+  font-size: 5em;
+  margin: 0.5em 0;
+  @media (max-width: 400px) {
+    font-size: 4em;
+  }
+`
+
+const Image = styled.img`
+  width: 10em;
+  margin-right: 2em;
+  @media (max-width: 600px) {
+    margin-right: 0;
+  }
+  @media (max-width: 400px) {
+    width: 8em;
+  }
+`
+
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <Link to="/page-2/">Go to page 2</Link>
+    <Container>
+      <Image src={logo} />
+      <Name>GitPub</Name>
+    </Container>
   </Layout>
 )
 
